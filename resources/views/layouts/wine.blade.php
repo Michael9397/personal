e49e99baf7117f57126c72510003ef5bd4151e75
Michael Claar <michael@piestar.com>
2024-12-08 18:56:47 -0600

    ready for launch maybe

--- resources/views/layouts/wine.blade.php
+++ resources/views/layouts/wine.blade.php
@@ -21,9 +21,13 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+                    <div class="max-w-7xl mx-auto py-6 px-10 sm:px-6 lg:px-8">
+                        <div class="flex justify-between items-center">
+                            {{ $header }}
+                            <livewire:welcome.navigation />
+                        </div>
                     </div>
+
                 </header>
             @endif
 
